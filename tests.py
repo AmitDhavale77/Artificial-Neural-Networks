adfe4be90e0dcf140fb6428bb0a10485aa317f0a
aps124 <aps124@ic.ac.uk>
2024-11-06 14:52:12 +0000

    Removing extra comments in tests.py

--- tests.py
+++ tests.py
@@ -4,52 +4,6 @@ import unittest
 
 from part1_nn_lib import LinearLayer, MSELossLayer
 
-# learning_rate = 0.0001
-# n_samples = 10
-# n_in = 2
-# n_out = 1
-# epochs = 100000
-
-# seed = 60012
-# rg = default_rng(seed)
-
-# weights = np.array([4, 2.5])
-# bias = np.ones((n_samples)) * 1.5
-# x = rg.random((n_samples, n_in))*10.0
-# y = np.matmul(x, weights) + bias
-
-# # add noise to y
-# # comment these out if you want to work with a perfectly clean dataset
-# # noise = rg.standard_normal(y.shape)
-# # y = y + noise
-
-# x_train = x
-# y_train = y.reshape(-1, 1)
-# # x_test = x[80:, :2]
-# # y_test = y[80:]
-
-# layer = LinearLayer(n_in, n_out)
-# loss = MSELossLayer()
-
-# print(layer._W)
-# print(layer._b)
-
-# for x in range(epochs):
-#     outputs = layer(x_train)
-#     cur_loss = loss.forward(outputs, y_train)
-
-#     if x % 500 == 0:
-#         print(f"Epoch: {x}. Loss: {cur_loss}")
-
-#     grad_loss_wrt_outputs = loss.backward()
-
-#     grad_loss_wrt_inputs = layer.backward(grad_loss_wrt_outputs)
-#     layer.update_params(learning_rate)
-
-# print(layer._W)
-# print(layer._b)
-
-# print(layer(inputs))
 
 class TestNeuralNetworkTraining(unittest.TestCase):
     def setUp(self):
@@ -107,6 +61,7 @@ class TestNeuralNetworkTraining(unittest.TestCase):
 
         np.testing.assert_almost_equal(learned_weights, expected_weights, decimal=1, err_msg="Learned weights do not match expected weights")
         self.assertAlmostEqual(learned_bias, expected_bias, places=1, msg="Learned bias does not match expected bias")
+
 
 if __name__ == '__main__':
     unittest.main()
