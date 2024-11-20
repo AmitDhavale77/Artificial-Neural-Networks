5eb86d07368e5f7f91be320da0ed8ad88b04af9a
Lauren Robson <91836994+la-robson@users.noreply.github.com>
2024-11-20 14:30:37 +0000

    Initial vis. of the hyperparameter impact

---
+++ plotting_hyperparam_tuning.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+if __name__ == "__main__":
+    # Load the data
+    data = pd.read_csv("hyperparameter_tuning_results.csv")
+
+    # describe the data
+    print(data.describe())
+
+    # Plot each hyperparameter against the r2 val and test score
+    hyperparameters = ["num_layers", "neurons", "batch_size", "epochs", "learning_rate"]
+    accuracy_metrics = [
+        "train_rmse",
+        "val_rmse",
+        "test_rmse",
+        "train_r2",
+        "val_r2",
+        "test_r2",
+    ]
+
+    # for hyperparameter in hyperparameters:
+    #     sns.scatterplot(data=data, x=hyperparameter, y="val_r2")
+    #     sns.scatterplot(data=data, x=hyperparameter, y="test_r2")
+    #     plt.title(f"Impact of {hyperparameter} on R2 score")
+    #     plt.legend(["Validation R2", "Test R2"])
+    #     plt.show()
+    plt.rcParams.update({"font.size": 12, "font.family": "serif"})
+    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+    axes = axes.flatten()
+    for i, hyperparameter in enumerate(hyperparameters):
+        sns.lineplot(
+            data=data, x=hyperparameter, y="val_r2", ax=axes[i], label="Validation"
+        )
+        sns.lineplot(data=data, x=hyperparameter, y="test_r2", ax=axes[i], label="Test")
+        sns.scatterplot(
+            data=data,
+            x=hyperparameter,
+            y="val_r2",
+            ax=axes[i],
+            alpha=0.1,
+            label=None,
+            marker="o",
+            s=5,
+        )
+        sns.scatterplot(
+            data=data,
+            x=hyperparameter,
+            y="test_r2",
+            ax=axes[i],
+            alpha=0.1,
+            label=None,
+            marker="o",
+            s=5,
+        )
+        axes[i].set_title(f"Impact of {hyperparameter} on R2 score")
+        axes[i].set_xlabel(hyperparameter)
+        axes[i].set_ylabel("R2 score")
+        axes[i].set_ylim(0.6, 0.85)
+        axes[i].legend(loc="lower right")
+
+    # Remove the empty subplot
+    fig.delaxes(axes[-1])
+    plt.tight_layout(pad=3.0)
+    plt.show()
